e48b249e03f28af057e63551b4bae473a70c217b
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-20 23:36:34 +0600

    data post and get mongodb

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 require('dotenv').config()
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
@@ -23,6 +23,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // create database collection
+    const jobsCollection=client.db('soloSphereJobs').collection('jobs')
+
+    // create a post method
+    app.post('/addJobs',async(req,res)=>{
+      const jobs=req.body;
+      console.log(jobs);
+      const result=await jobsCollection.insertOne(jobs)
+      res.send(result)
+    })
+
+    // data get on the mongodb
+    app.get('/Jobs',async(req,res)=>{
+      const result=await jobsCollection.find().toArray()
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
